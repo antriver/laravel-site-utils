09e3c74cf604892e3fe01ecedc519676a353e83c
Anthony Kuske <anthonykuske@gmail.com>
2020-01-02 23:41:35 +0000

    Change model presenter signature

--- src/ModelPresenters/Base/ModelPresenterInterface.php
+++ src/ModelPresenters/Base/ModelPresenterInterface.php
@@ -2,17 +2,18 @@
 
 namespace Antriver\LaravelSiteUtils\ModelPresenters\Base;
 
+use Antriver\LaravelSiteUtils\Models\Base\AbstractModel;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 
 interface ModelPresenterInterface
 {
     /**
-     * @param Model $model
+     * @param AbstractModel $model
      *
      * @return array|null
      */
-    public function present(Model $model): ?array;
+    public function present(AbstractModel $model): ?array;
 
     /**
      * @param Model[]|\Iterator|Collection $models
