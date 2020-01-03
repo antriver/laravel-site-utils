7eb68e7bf00e11140ce0b0ae356c63a3ef1868af
Anthony Kuske <anthonykuske@gmail.com>
2020-01-03 01:22:12 +0000

    Better paginator toArray

--- src/Pagination/LengthAwarePaginator.php
+++ src/Pagination/LengthAwarePaginator.php
@@ -102,4 +102,27 @@ class LengthAwarePaginator extends \Illuminate\Pagination\LengthAwarePaginator
         // Note that UrlWindow when used here is our custom one (because of the namespace)
         return UrlWindow::make($this);
     }
+
+    /**
+     * Get the instance as an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return [
+            'currentPage' => $this->currentPage(),
+            'data' => $this->items->toArray(),
+            'firstPageUrl' => $this->url(1),
+            'from' => $this->firstItem(),
+            'lastPage' => $this->lastPage(),
+            'lastPageUrl' => $this->url($this->lastPage()),
+            'nextPageUrl' => $this->nextPageUrl(),
+            'path' => $this->path,
+            'perPage' => $this->perPage(),
+            'prevPageUrl' => $this->previousPageUrl(),
+            'to' => $this->lastItem(),
+            'total' => $this->total(),
+        ];
+    }
 }
