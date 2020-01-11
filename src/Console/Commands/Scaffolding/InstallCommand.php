fffff65cfc2e6c7ffc848d42c420e2df5c585dc6
Anthony Kuske <anthonykuske@gmail.com>
2020-01-11 02:31:34 +0000

    Add install command

--- src/Console/Commands/Scaffolding/InstallCommand.php
+++ src/Console/Commands/Scaffolding/InstallCommand.php
@@ -3,11 +3,6 @@
 namespace Antriver\LaravelSiteScaffolding\Console\Commands\Scaffolding;
 
 use Antriver\LaravelSiteScaffolding\Console\Commands\AbstractCommand;
-use Antriver\LaravelSiteScaffolding\Http\Controllers\ControllerTrait;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
-use SplFileInfo;
-use Symfony\Component\Finder\Finder;
 
 class InstallCommand extends AbstractCommand
 {
@@ -27,17 +22,27 @@ class InstallCommand extends AbstractCommand
      */
     protected $description = 'Create default files.';
 
+    protected $traitsForApiControllers = [
+        \Antriver\LaravelSiteScaffolding\Auth\Http\AuthControllerTrait::class => 'AuthController',
+        \Antriver\LaravelSiteScaffolding\EmailVerification\Http\EmailVerificationControllerTrait::class => 'EmailVerificationController',
+        \Antriver\LaravelSiteScaffolding\Auth\Http\ForgotPasswordControllerTrait::class => 'ForgotPasswordController',
+        \Antriver\LaravelSiteScaffolding\Auth\Http\RegisterControllerTrait::class => 'RegisterController',
+        \Antriver\LaravelSiteScaffolding\Auth\Http\ResetPasswordControllerTrait::class => 'ResetPasswordController'
+    ];
+
     /**
      * Execute the console command.
      */
     public function handle()
     {
         $this->makeAbstractController();
+        $this->makeAbstractApiController();
+        $this->makeApiControllersForTraits();
     }
 
     protected function makeAbstractController()
     {
-        $namespace = $this->getAppNamespace().'\\Http\\Controllers';
+        $namespace = $this->getAppNamespace().'\Http\Controllers';
         $path = app()->basePath().'/app/Http/Controllers/AbstractController.php';
         $contents = <<<EOL
 <?php
@@ -50,6 +55,58 @@ use Illuminate\Routing\Controller
 abstract class AbstractController extends Controller 
 {
     use ControllerTrait;
+}
+
+EOL;
+        $this->writeFileIfNotExists($path, $contents);
+    }
+
+    protected function makeAbstractApiController()
+    {
+        $namespace = $this->getAppNamespace().'\Http\Controllers\Api';
+        $path = app()->basePath().'/app/Http/Controllers/ApiAbstractApiController.php';
+        $contents = <<<EOL
+<?php
+
+namespace {$namespace};
+
+use Antriver\LaravelSiteScaffolding\Http\Controllers\ApiControllerTrait;
+use {$this->getAppNamespace()}\Http\Controllers\AbstractController;
+
+abstract class AbstractApiController extends AbstractController
+{
+    use ApiControllerTrait;
+}
+
+EOL;
+        $this->writeFileIfNotExists($path, $contents);
+    }
+
+    protected function makeApiControllersForTraits()
+    {
+        foreach ($this->traitsForApiControllers as $traitClass => $controllerName) {
+            $this->makeApiControllesForTraits($traitClass => $controllerName);
+        }
+    }
+
+    protected function makeApiControllesForTraits(string $traitClass, string $controllerName)
+    {
+        $traitName = explode('\\', $traitClass);
+        $traitName = array_pop($traitName);
+
+        $namespace = $this->getAppNamespace().'\Http\Controllers\Api';
+        $path = app()->basePath().'/app/Http/Controllers/'.$controllerName.'.php';
+        $contents = <<<EOL
+<?php
+
+namespace {$namespace};
+
+use {$traitClass};
+use {$this->getAppNamespace()}\Http\Controllers\Api\AbstractApiController;
+
+abstract class {$controllerName} extends AbstractController
+{
+    use {$traitName};
 }
 
 EOL;
--- src/Console/Commands/Scaffolding/MakesClassesTrait.php
+++ src/Console/Commands/Scaffolding/MakesClassesTrait.php
@@ -16,6 +16,14 @@ trait MakesClassesTrait
 
     protected function writeFileIfNotExists($path, $contents)
     {
+        $dir = explode('/', $path);
+        array_pop($dir);
+        $dir = implode('/', $dir);
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777, true);
+            $this->info("Created directory {$dir}");
+        }
+
         if (!file_exists($path)) {
             file_put_contents($path, $contents);
             $this->info("Wrote {$path}");
