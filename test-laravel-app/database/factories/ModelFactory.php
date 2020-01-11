5f114d2049a1e2d50e057afbacd02162d4671c8a
Anthony Kuske <anthonykuske@gmail.com>
2020-01-11 20:02:19 +0000

    CreatePasswordResetTokensTable

--- test-laravel-app/database/factories/ModelFactory.php
+++ test-laravel-app/database/factories/ModelFactory.php
@@ -2,21 +2,15 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use Antriver\LaravelSiteScaffolding\EmailVerification\EmailVerification;
 use Faker\Generator;
 
-$factory->define(\Antriver\LaravelSiteScaffolding\Users\User::class, function (Generator $faker) {
-    return [
-        'username' => $faker->userName,
-        'email' => $faker->unique()->safeEmail,
-        'password' => bcrypt('secret'),
-    ];
-});
-
-$factory->define(\Antriver\LaravelSiteScaffolding\EmailVerification\EmailVerification::class, function (Generator $faker) {
-    return [
-        'email' => $faker->unique()->safeEmail,
-        'token' => uniqid(),
-        'type' => EmailVerification::TYPE_SIGNUP
-    ];
-});
+$factory->define(
+    \Antriver\LaravelSiteScaffolding\Users\User::class,
+    function (Generator $faker) {
+        return [
+            'username' => $faker->userName,
+            'email' => $faker->unique()->safeEmail,
+            'password' => bcrypt('secret'),
+        ];
+    }
+);
